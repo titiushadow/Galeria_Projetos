2dc43a6883e8fed33f61fd64638ee533264cb079
titiushadow <mesaque1212@gmail.com>
2023-03-01 19:17:18 -0300

    Finalizando galeria de projetos

---
+++ js/script.js
@@ -0,0 +1,180 @@
+// Nubank
+function nubank() {
+    Swal.fire({
+        title: "Nubank",
+        html: '<p>Projeto foi desenvolvido para praticar HTML5 e CSS3, o projeto possui algumas mudanças na tela de login.</p>' +
+        '<a class="color" href="https://master-nubank_clone-titiushadow.bohr.io/" target="_blank">Ver projeto</a>',
+        imageUrl: "https://static.poder360.com.br/2022/09/logo-nubank-848x477.jpg",
+        imageWidth: 400,
+        imageHeight: 270,
+        imageAlt: 'Nubank',
+        confirmButtonColor: '#3085d6',
+        position: 'center',
+    })
+}
+
+// Prime video
+function prime() {
+    Swal.fire({
+        title: "Prime Video",
+        html: '<p>Projeto foi desenvolvido para praticar HTML5 e CSS3, o projeto possui algumas mudanças na tela de login.</p>' +
+        '<a class="color" href="https://master-prime_video-titiushadow.bohr.io/" target="_blank">Ver projeto</a>',
+        imageUrl: "https://m.media-amazon.com/images/G/01/primevideo/seo/primevideo-seo-logo.png",
+        imageWidth: 400,
+        imageHeight: 270,
+        imageAlt: 'Prime-video',
+        confirmButtonColor: '#3085d6',
+        position: 'center',
+    })
+}
+
+// Modal Facebook
+function facebook() {
+    Swal.fire({
+        title: "Facebook",
+        html: '<p>Projeto foi desenvolvido para praticar HTML5 e CSS3, o projeto possui algumas mudanças na tela de login.</p>' +
+        '<a class="color" href="https://master-facebook_clone-titiushadow.bohr.io/" target="_blank">Ver projeto</a>',
+        imageUrl: "https://webcompany.com.br/wp-content/uploads/2021/07/facebook-no-brasil.jpg",
+        imageWidth: 400,
+        imageHeight: 270,
+        imageAlt: 'Facebook',
+        confirmButtonColor: '#3085d6',
+        position: 'center',
+    })
+}
+
+// Modal IMC
+function imc() {
+    Swal.fire({
+        title: "IMC",
+        html: '<p>Projeto foi desenvolvido para praticar HTML5 e CSS3, o projeto possui algumas mudanças na tela de login.</p>' +
+        '<a class="color" href="https://master-calcular-imc-titiushadow.bohr.io/" target="_blank">Ver projeto</a>',
+        imageUrl: "https://user-images.githubusercontent.com/63453751/197347971-0f064109-e070-4c27-8295-a918fb59df1c.png",
+        imageWidth: 400,
+        imageHeight: 270,
+        imageAlt: 'IMC',
+        confirmButtonColor: '#3085d6',
+        position: 'center',
+    })
+}
+
+// Modal Parallax
+function parallax() {
+    Swal.fire({
+        title: "Efeito Parallax",
+        html: '<p>Projeto foi desenvolvido para praticar HTML5 e CSS3, o projeto possui algumas mudanças na tela de login.</p>' +
+        '<a class="color" href="https://master-parallax-titiushadow.bohr.io/" target="_blank">Ver projeto</a>',
+        imageUrl: "https://www.10wallpaper.com/wallpaper/1366x768/1706/House_mountains_snow_lake-High_Quality_Wallpaper_1366x768.jpg",
+        imageWidth: 400,
+        imageHeight: 270,
+        imageAlt: 'Efeito Parallax',
+        confirmButtonColor: '#3085d6',
+        position: 'center',
+    })
+}
+
+// Make Your Burger
+function burger() {
+    Swal.fire({
+        title: "Make Your Burger",
+        html: '<p>Projeto foi desenvolvido para praticar HTML5 e CSS3, o projeto possui algumas mudanças na tela de login.</p>' +
+        '<a class="color" href="https://make_your_burger-titiushadow.bohr.io/" target="_blank">Ver projeto</a>',
+        imageUrl: "https://user-images.githubusercontent.com/63453751/162729808-f568227c-41fb-4ad5-9841-840d535b26d2.png",
+        imageWidth: 400,
+        imageHeight: 270,
+        imageAlt: 'Make Your Burger',
+        confirmButtonColor: '#3085d6',
+        position: 'center',
+    })
+}
+
+// Slider
+function slider() {
+    Swal.fire({
+        title: "Slider",
+        html: '<p>Projeto foi desenvolvido para praticar HTML5 e CSS3, o projeto possui algumas mudanças na tela de login.</p>'+ 
+        '<a class="color" href="https://slider.bohr.io" target="_blank">Ver projeto</a>',
+        imageUrl: "/img/slider.png",
+        imageWidth: 400,
+        imageHeight: 270,
+        imageAlt: 'Make Your Burger',
+        confirmButtonColor: '#3085d6',
+        position: 'center',
+    })
+}
+
+// Tela de Registro
+function register() {
+    Swal.fire({
+        title: "Tela de Registro",
+        html: '<p>Projeto foi desenvolvido para praticar HTML5 e CSS3, o projeto possui algumas mudanças na tela de login.</p>' + 
+        '<a class="color" href="https://loginregister.bohr.io" target="_blank">Ver projeto</a>',
+        imageUrl: "/img/register.png",
+        imageWidth: 400,
+        imageHeight: 270,
+        imageAlt: 'Make Your Burger',
+        confirmButtonColor: '#3085d6',
+        position: 'center',
+    })
+}
+
+//Halloween
+function halloween() {
+    Swal.fire({
+        title: "Halloween",
+        html: '<p>Projeto foi desenvolvido para praticar HTML5 e CSS3, o projeto possui algumas mudanças na tela de login.</p>' + 
+        '<a class="color" href="https://halloween.bohr.io" target="_blank">Ver projeto</a>',
+        imageUrl: "/img/halloween.png",
+        imageWidth: 400,
+        imageHeight: 270,
+        imageAlt: 'Make Your Burger',
+        confirmButtonColor: '#3085d6',
+        position: 'center',
+    })
+}
+
+// Tela de Login
+function login() {
+    Swal.fire({
+        title: "Tela de login",
+        html: '<p>Projeto foi desenvolvido para praticar HTML5 e CSS3, o projeto possui algumas mudanças na tela de login.</p>'  + 
+        '<a class="color" href="https://pagelogin.bohr.io" target="_blank">Ver projeto</a>',
+        imageUrl: "/img/login-first.png",
+        imageWidth: 400,
+        imageHeight: 270,
+        imageAlt: 'Make Your Burger',
+        confirmButtonColor: '#3085d6',
+        position: 'center',
+    })
+}
+
+// Relógio Dinamico
+function clock() {
+    Swal.fire({
+        title: "Relógio dinamico",
+        html: '<p>Projeto foi desenvolvido para praticar HTML5 e CSS3, o projeto possui algumas mudanças na tela de login.</p>' + 
+        '<a class="color" href="https://relogiodinamico.bohr.io" target="_blank">Ver projeto</a>',
+        imageUrl: "/img/clock.png",
+        imageWidth: 400,
+        imageHeight: 270,
+        imageAlt: 'Make Your Burger',
+        confirmButtonColor: '#3085d6',
+        position: 'center',
+    })
+}
+
+// Login divider
+function login_divider() {
+    Swal.fire({
+        title: "Tela de registro moderna",
+        html: '<p>Projeto foi desenvolvido para praticar HTML5 e CSS3, o projeto possui algumas mudanças na tela de login.</p>' + 
+        '<a class="color" href="https://formulario.bohr.io" target="_blank">Ver projeto</a>',
+        imageUrl: "/img/form.png",
+        imageWidth: 380,
+        imageHeight: 270,
+        imageAlt: 'Make Your Burger',
+        confirmButtonColor: '#3085d6',
+        position: 'center',
+    })
+}
+
